59a1b92a43de550027d21eda7cb1b4ff7ef4e32a
Serhii Kroshka <pm012@ukr.net>
2025-07-29 01:25:47 +0300

    Corrected naming and location of project directory to simplify its further deploy

--- connect_care_admin/asgi.py
+++ connect_care_admin/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for best_cms project.
+ASGI config for connect_care_admin project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'best_cms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'connect_care_admin.settings')
 
 application = get_asgi_application()
--- connect_care_admin/urls.py
+++ connect_care_admin/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for best_cms project.
+URL configuration for connect_care_admin project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
-    path('cms/', include('cms.urls')),
     path('admin/', admin.site.urls),
 ]
